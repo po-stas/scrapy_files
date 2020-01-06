86f3e13bd0ff09f142c889866933fcca0bce79c3
postas <pologrudov@gmail.com>
2020-01-06 08:20:00 -0700

    Links extracted

--- youlaparser/spiders/youla.py
+++ youlaparser/spiders/youla.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class YoulaSpider(scrapy.Spider):
-    name = 'youla'
+    name = 'youla.auto'
     allowed_domains = ['youla.ru']
-    start_urls = ['http://youla.ru/']
+    start_urls = ['https://auto.youla.ru/rossiya/cars/used/?vinChecked=1']
 
-    def parse(self, response):
+    def parse(self, response:HtmlResponse):
+        links = response.xpath('//a[@class="SerpSnippet_name__3F7Yu SerpSnippet_titleText__1Ex8A blackLink"]/@href').extract()
+        for link in links:
+            yield response.follow(link, callback=self.parse_links)
+
+    def parse_links(self, response:HtmlResponse):
         pass
+
+# class="SerpSnippet_name__3F7Yu SerpSnippet_titleText__1Ex8A blackLink"
+# target="_blank" data-target="serp-snippet-title" title="Nissan X-Trail с пробегом">Nissan X-Trail, T31 [рестайлинг]</a>
